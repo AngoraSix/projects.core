22d41baa8cede44019b4e348274f0d90dfc1efca
Ger Roza <rozagerardo@gmail.com>
2022-01-06 23:37:36 -0300

    Creating Project using contributorId value extracted from Header, and IDE reformatted code. Still we should analyze edge cases, error management and parametrize header name

--- src/main/kotlin/com/angorasix/projects/core/presentation/dto/Dto.kt
+++ src/main/kotlin/com/angorasix/projects/core/presentation/dto/Dto.kt
@@ -8,25 +8,30 @@ import java.time.ZonedDateTime
  * @author rozagerardo
  */
 data class AttributeDto(
-    val key: String,
-    val value: String,
+        val key: String,
+        val value: String,
 )
 
 data class ProjectDto(
-    var id: String? = null,
-    var name: String? = null,
-    var attributes: MutableSet<AttributeDto> = mutableSetOf(),
-    var requirements: MutableSet<AttributeDto> = mutableSetOf(),
-    var creatorId: String? = null,
-    var createdAt: ZonedDateTime? = null
+        var id: String? = null,
+        var name: String? = null,
+        var attributes: MutableSet<AttributeDto> = mutableSetOf(),
+        var requirements: MutableSet<AttributeDto> = mutableSetOf(),
+        var creatorId: String? = null,
+        var createdAt: ZonedDateTime? = null
 ) {
 
     constructor(
-        name: String,
-        attributes: MutableSet<AttributeDto> = mutableSetOf<AttributeDto>()
+            name: String,
+            attributes: MutableSet<AttributeDto> = mutableSetOf<AttributeDto>()
     ) : this(
-        null,
-        name,
-        attributes,
+            null,
+            name,
+            attributes,
     )
 }
+
+data class ContributorHeaderDto(
+        var contributorId: String,
+        var attributes: Map<String, String> = mutableMapOf(),
+)
